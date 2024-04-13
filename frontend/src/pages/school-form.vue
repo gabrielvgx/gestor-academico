22c18bf00619b607b8564549d1eec867f01e0758
Gabriel Victor <gabrielvgx@gmail.com>
2024-04-13 17:02:09 -0300

    Inclusao de telas de cadastro de usuario e cadastro de escola-turma

---
+++ frontend/src/pages/school-form.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-container class='pa-0 w-100 h-100 d-flex generic-container'>
+    <v-form class="school-form d-flex flex-column generic-form" ref="form">
+      <v-row class="form-title">
+        <h3>Cadastro de Escola / Turma</h3>
+      </v-row>
+      <div class="form-body">
+        <div>
+          <div class="text-subtitle-1 required">Nome da Escola</div>
+          <v-text-field
+              v-model="formData.schoolName"
+              :rules="rules"
+              type="text"
+              density="compact"
+              placeholder="Ex.: EMEI Nova Lima"
+              variant="outlined"
+          ></v-text-field>
+        </div>
+        <div v-for="(_, idx) in formData.classCollection" v-bind:key="`class_${idx}`">
+          <div class="text-subtitle-1 required">Nome da Turma {{ formData.classCollection.length > 1 ? idx + 1 : ''  }}</div>
+          <div class="d-flex">
+            <v-text-field
+                v-model="formData.classCollection[idx]"
+                :rules="rules"
+                type="text"
+                density="compact"
+                placeholder="Ex.: Novo Amanhecer"
+                variant="outlined"
+            ></v-text-field>
+            <v-btn class="ms-3" color="success" icon="mdi-plus" density="comfortable" @click="() => formData.classCollection.push(null)"></v-btn>
+            <v-btn v-if="idx > 0" class="ms-2" color="error" icon="mdi-delete-outline" density="comfortable" @click="() => formData.classCollection.splice(idx, 1)"></v-btn>
+          </div>
+        </div>
+      </div>
+      <v-btn color="primary" @click="() => console.log(formData.classCollection)">Confirmar</v-btn>
+    </v-form>
+  </v-container>
+</template>
+<script lang="js">
+import { ref } from 'vue';
+import Rule from '@/util/Rule';
+import Login from '@/controllers/Login';
+
+export default {
+  name: 'school-form',
+  components: {},
+  methods: {
+    async login() {
+      const isValid = await this.$refs.form.validate();
+      if (isValid) {
+        Login.auth(this.formData);
+      }
+    },
+  },
+  setup() {
+    const visible = ref(false);
+    const formData = ref({
+      schoolName: null,
+      classCollection: [null],
+    });
+    return {
+      visible,
+      rules: [
+        Rule.required(),
+      ],
+      formData,
+    }
+  }
+}
+</script>
+<style lang="scss">
+.form-body {
+  overflow-y: auto;
+  max-height: 75vh;
+}
+</style>
---
+++ frontend/src/pages/user-form.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-container class='user-container generic-container'>
+    <v-form class="user-form generic-form" ref="form">
+      <v-row class="form-title">
+        <h3>Cadastro de Usuário</h3>
+      </v-row>
+      <div>
+        <div class="text-subtitle-1 required">Nome</div>
+        <v-text-field
+            v-model="formData.name"
+            :rules="rules"
+            type="text"
+            density="compact"
+            placeholder="Nome"
+            variant="outlined"
+            autocomplete="name"
+        ></v-text-field>
+      </div>
+      <div>
+        <div class="text-subtitle-1 required">E-mail</div>
+        <v-text-field
+          v-model="formData.email"
+          :rules="rules"
+          type="text"
+          density="compact"
+          placeholder="E-mail"
+          variant="outlined"
+          autocomplete="email"
+        ></v-text-field>
+      </div>
+      <div>
+        <v-select
+          class="required"
+          :rules="rules"
+          label="Cargo"
+          :items="['Professor(a)', 'Coordenador(a) / Diretor(a)', 'Profissional da Cozinha']"
+          density="comfortable"
+          variant="underlined"
+        ></v-select>
+      </div>
+      <div>
+        <v-select
+          class="required"
+          :rules="rules"
+          label="Escola"
+          :items="['EMEI Alto Vera Cruz', 'EMEI Teste1', 'EMEI Teste2',]"
+          density="comfortable"
+          variant="underlined"
+        ></v-select>
+      </div>
+      <div>
+        <v-select
+          class="required"
+          :rules="rules"
+          label="Turma"
+          :items="['Turma dos Amigos 5/6 Anos', 'Turma Beija Flor 3/4 Anos',]"
+          density="comfortable"
+          variant="underlined"
+        ></v-select>
+      </div>
+      <section class="inputs d-flex flex-column">
+        <v-row>
+
+        </v-row>
+        <v-row>
+
+        </v-row>
+      </section>
+      <v-btn color="primary">Confirmar</v-btn>
+    </v-form>
+  </v-container>
+</template>
+<script lang="js">
+import { ref } from 'vue';
+import Rule from '@/util/Rule';
+import Login from '@/controllers/Login';
+
+export default {
+  name: 'user-form',
+  components: {},
+  methods: {
+    async login() {
+      const isValid = await this.$refs.form.validate();
+      if (isValid) {
+        Login.auth(this.formData);
+      }
+    },
+  },
+  setup() {
+    const visible = ref(false);
+    const formData = ref({
+      name: null,
+      email: null,
+    });
+    return {
+      visible,
+      rules: [
+        Rule.required(),
+      ],
+      formData,
+    }
+  }
+}
+</script>
+<style lang="scss">
+</style>
